07ec194c5334da4152287ef21ec8868465a2de61
Mike Pickering <mpicker0@gmail.com>
2017-12-03 23:13:29 +0000

    Solve first Dec 3 problem; still need to clean up code

--- scripts/dec03.exs
+++ scripts/dec03.exs
@@ -1,4 +1,6 @@
 input = 289326
 
+#AC.Dec03.test_it()
+
 IO.puts(:io_lib.format "Solution for problem 1: ~B", [AC.Dec03.distance(input)])
 #IO.puts(:io_lib.format "Solution for problem 2: ~B", [AC.Dec03.distance_2(input)])
--- test/ac/dec03_test.exs
+++ test/ac/dec03_test.exs
@@ -19,6 +19,18 @@ defmodule AC.Dec03Test do
     assert AC.Dec03.distance(1024) == 31
   end
 
+  test "test no up component" do
+    assert AC.Dec03.distance(2) == 1
+  end
+
+  test "test no left component" do
+    assert AC.Dec03.distance(3) == 2
+  end
+
+  test "test no down component" do
+    assert AC.Dec03.distance(5) == 2
+  end
+
   # problem 2
 
 end
