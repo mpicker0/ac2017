b18125cf53ab0528d9ad7c7438bd3d2e3fceae98
Mike Pickering <mpicker0@gmail.com>
2017-12-04 23:13:27 +0000

    Finish Dec 4 problem 2

--- lib/ac/dec04.ex
+++ lib/ac/dec04.ex
@@ -18,13 +18,24 @@ defmodule AC.Dec04 do
     count
   end
 
+  def canonicalize_word(word) do
+    Enum.sort(String.codepoints(word))
+  end
+
   def valid_passphrase_2(passphrase) do
-    IO.puts("Got passphrase " <> passphrase)
-    :false
+    matching_words = String.split(passphrase)
+    |> map(fn(w) -> canonicalize_word(w) end)
+    |> reduce(%{}, fn (x, acc) -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    |> Map.values
+    |> find(0, fn(count) -> count > 1 end)
+    matching_words == 0
   end
 
   def valid_passphrases_2(filename) do
-    IO.puts("Reading file " <> filename)
-    -1
+    count = File.stream!(filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(fn (line) -> valid_passphrase_2(line) end)
+    |> Enum.count(fn (item) -> item end)
+    count
   end
 end
