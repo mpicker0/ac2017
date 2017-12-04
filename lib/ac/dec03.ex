143ac15e7fcb0bd6968b652e4fd5503e002e9c33
Mike Pickering <mpicker0@gmail.com>
2017-12-04 05:44:16 +0000

    Start Dec 3 problem 2; still not working

--- lib/ac/dec03.ex
+++ lib/ac/dec03.ex
@@ -1,35 +1,97 @@
 defmodule AC.Dec03 do
 
-  def even_stream() do
+  def even_stream do
     Stream.iterate(0, & &1 + 2)
   end
 
+  def moves do
+    import List
+    flatten(
+      for times <- Enum.to_list(even_stream() |> Enum.take(600)),
+      do: duplicate(:l, times) ++ duplicate(:d, times) ++
+          duplicate(:r, times + 1) ++ duplicate(:u, times + 1)
+    )
+  end
+
+  def get_and_log(spiral_map, coords, default) do
+    x = elem(coords, 0)
+    y = elem(coords, 1)
+    IO.puts(:io_lib.format " x: ~B, y: ~B", [x,y])
+    Map.get(spiral_map, {x, y}, 0)
+  end
+
+  def adjacent_values(spiral_map, coords) do
+    IO.puts("-----")
+    IO.puts("called adjacent_values with spiral_map of")
+    IO.inspect(spiral_map)
+    IO.puts("and coords of")
+    IO.inspect(coords)
+    all_things = for x <- elem(coords, 0) - 1 .. elem(coords, 0) + 1,
+        y <- elem(coords, 1) - 1 .. elem(coords, 0) + 1,
+        do:
+        #Map.get(spiral_map, {x, y}, 0)
+        get_and_log(spiral_map, {x, y}, 0)
+
+    all_things
+    |> IO.inspect()
+    |> Enum.sum
+  end
+
+  def coords_from_movement(location) do
+    movement_map =
+      moves()
+      |> Enum.take(location - 1)
+      |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    { Map.get(movement_map, :r, 0) - Map.get(movement_map, :l, 0),
+      Map.get(movement_map, :u, 0) - Map.get(movement_map, :d, 0) }
+  end
+
+  def spiral_values do
+    # initial coordinates with respect to origin are {0, 0}
+    # first value is 1
+    # add coordinates and value to a map:  {0, 0} => 1
+    value_map = %{{0, 0} => 1}
+
+    # next coordinates can be calculated based on moves: {1, 0}
+    # second value is (sum of all adjacent squares)
+    # add coordinates to a map:  {1, 0} =>
+    coord_list = for l <- 1..10,  do:
+      coords_from_movement(l)
+
+    IO.inspect(coord_list)
+
+    coord_map = coord_list
+      |> Enum.reduce(value_map, fn(x, acc) ->
+           Map.put(acc, x, adjacent_values(acc, x))
+         end)
+    #IO.inspect(coord_map)
+
+    coord_list
+      |> Enum.map(fn(x) -> Map.get(coord_map, x) end)
+      |> IO.inspect
+  end
+
   def sum_at(location) do
-    location
+    Enum.at(spiral_values(), location - 1)
   end
 
   # User-facing functions
 
   def distance_from_origin(location) do
-    import List
-    result = flatten(
-      for times <- Enum.to_list(even_stream() |> Enum.take(600)),
-      do: duplicate(:l, times) ++ duplicate(:d, times) ++
-          duplicate(:r, times + 1) ++ duplicate(:u, times + 1)
-    )
-
-    movement =
-      result
+    movement_map =
+      moves()
       |> Enum.take(location - 1)
       |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
 
-     dist_x = abs(Map.get(movement, :r, 0) - Map.get(movement, :l, 0))
-     dist_y = abs(Map.get(movement, :u, 0) - Map.get(movement, :d, 0))
-     dist_x + dist_y
+    dist_x = abs(Map.get(movement_map, :r, 0) - Map.get(movement_map, :l, 0))
+    dist_y = abs(Map.get(movement_map, :u, 0) - Map.get(movement_map, :d, 0))
+    dist_x + dist_y
   end
 
   def first_greater(num) do
-    num
+    # Get a list of all the numbers
+    # Report back the first one greater than num
+    Enum.find(spiral_values(), fn(x) -> x > num end)
   end
 
 end
--- test/ac/dec03_test.exs
+++ test/ac/dec03_test.exs
@@ -33,11 +33,39 @@ defmodule AC.Dec03Test do
 
   # problem 2
 
-  test "first greater than 3 is 4" do
-    assert AC.Dec03.first_greater(3) == 4
-  end
+  # test "first greater than 3 is 4" do
+  #   assert AC.Dec03.first_greater(3) == 4
+  # end
+  #
+  # test "first greater than 330 is 351" do
+  #   assert AC.Dec03.first_greater(330) == 351
+  # end
 
   # support
+
+  test "adjacent_values sums correctly with a full map" do
+    matrix = %{ {-1,  1} => 1, {0,  1} => 1, {1,  1} => 1,
+                {-1,  0} => 1, {0,  0} => 0, {1,  0} => 1,
+                {-1, -1} => 1, {0, -1} => 1, {1, -1} => 1
+              }
+    assert AC.Dec03.adjacent_values(matrix, {0, 0}) == 8
+  end
+
+  test "adjacent_values sums correctly with a different starting point" do
+    matrix = %{ {-1,  1} => 1, {0,  1} => 1, {1,  1} => 1,
+                {-1,  0} => 1, {0,  0} => 0, {1,  0} => 0,
+                {-1, -1} => 1, {0, -1} => 1, {1, -1} => 1
+              }
+    assert AC.Dec03.adjacent_values(matrix, {1, 0}) == 4
+  end
+
+  test "adjacent_values sums correctly with a partial map" do
+    matrix = %{
+                               {0,  0} => 0, {1,  0} => 4,
+                                             {1, -1} => 1
+              }
+    assert AC.Dec03.adjacent_values(matrix, {0, 0}) == 5
+  end
 
   test "value at position 1" do
     assert AC.Dec03.sum_at(1) == 1
@@ -47,11 +75,19 @@ defmodule AC.Dec03Test do
     assert AC.Dec03.sum_at(2) == 1
   end
 
+  test "value at position 4" do
+    assert AC.Dec03.sum_at(4) == 4
+  end
+
+  test "value at position 5" do
+    assert AC.Dec03.sum_at(5) == 5    
+  end
+
   test "value at position 6" do
     assert AC.Dec03.sum_at(6) == 10
   end
-
-  test "value at position 23" do
-    assert AC.Dec03.sum_at(23) == 806
-  end
+  #
+  # test "value at position 23" do
+  #   assert AC.Dec03.sum_at(23) == 806
+  # end
 end
