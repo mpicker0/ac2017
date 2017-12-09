dd67972b2778c94fbb0c2d90bf8f1710f8bee261
Mike Pickering <mpicker0@gmail.com>
2017-12-09 00:30:06 +0000

    Start implementing Dec 7 part 2

--- lib/ac/dec07.ex
+++ lib/ac/dec07.ex
@@ -2,14 +2,18 @@ defmodule AC.Dec07 do
 
   # Part 1
   defmodule TreeNode do
-    defstruct name: nil, weight: -1, children: nil
+    defstruct name: nil, weight: -1, children: []
   end
 
   def parse_line(line) do
     if line =~ "->" do
       %{"name" => name, "weight" => weight, "children" => children} =
         Regex.named_captures(~r/(?<name>\w+) \((?<weight>\d+)\) -> (?<children>.*)$/, line)
-      %TreeNode{name: name, weight: String.to_integer(weight), children: children}
+      %TreeNode{
+        name: name,
+        weight: String.to_integer(weight),
+        children: Enum.map(String.split(children, ","), fn(c) -> String.trim(c) end)
+      }
     else
       %{"name" => name, "weight" => weight} =
         Regex.named_captures(~r/(?<name>\w+) \((?<weight>\d+)\)$/, line)
@@ -17,32 +21,29 @@ defmodule AC.Dec07 do
     end
   end
 
-  def process_node(node) do
-    IO.inspect(node)
-  end
-
   def find_root_in_list(nodes) do
     # For each TreeNode seen:
     #   put the Name in a set of known_nodes
     #   for each child seen, put the put the children's names in a map of known_children
-    # The root is the (only) node that is not a child (set difference known_nodes - known_children)
     acc_init = {MapSet.new(), MapSet.new()}
     {known_nodes, known_children} =
-      #nodes
-      #|> Enum.map(fn(node) -> process_node(node) end)
-      Enum.reduce(nodes, acc_init, fn(node, acc) ->
-        #MapSet.put(known_nodes, node.name)
-        #MapSet.put(known_children, "TODO")
-        IO.puts("considering node #{node.name} with children #{node.children}")
-        # TODO why can't I pattern match these?
-        known_nodes = elem(acc, 0)
-        known_children = elem(acc, 1)
-
-        # TODO iterate over the children and put them all in (another reduce)
-        {MapSet.put(known_nodes, node.name), MapSet.put(known_children, "known child seen")}
+      Enum.reduce(nodes, acc_init, fn(node, {known_nodes, known_children}) ->
+        {
+          MapSet.put(known_nodes, node.name),
+          Enum.reduce(node.children, known_children, fn(child, acc) ->
+            MapSet.put(acc, child)
+          end)
+        }
       end)
-      IO.inspect(known_nodes)
-      IO.inspect(known_children)
+    # The root is the (only) node that is not a child (set difference
+    # known_nodes - known_children)
+    # sanity check
+    if MapSet.size(known_nodes) - MapSet.size(known_children) != 1 do
+      raise "Could not determine the root!  Bug!"
+    end
+    MapSet.difference(known_nodes, known_children)
+    |> MapSet.to_list
+    |> hd
   end
 
   def find_root(filename) do
@@ -51,5 +52,53 @@ defmodule AC.Dec07 do
     |> Stream.map(fn(line) -> parse_line(line) end)
     |> Enum.to_list
     |> find_root_in_list()
+  end
+
+  # Part 2
+
+  # starting at "start" find the first (presumably only) child that is
+  # unbalanced in "tree"
+  def find_unbalanced_node(tree, start) do
+    this_node = Map.get(tree, start)
+    IO.puts("this_node:")
+    IO.inspect(this_node)
+    # if I'm balanced (that is, all my children are balanced)
+    if Enum.uniq_by(this_node.children, fn(c) ->
+      IO.puts("considering #{c}")
+      Map.get(tree, c).weight
+      end) == 1 do
+    #   find_unbalanced_node on each of my children
+      Enum.map(this_node.children, fn(c) -> find_unbalanced_node(tree, c) end)
+    else
+    # if I'm not balanced
+    #   return myself (start)
+      start
+    end
+  end
+
+  # one of node's children has an incorrect weight; find it
+  def find_erroneous_child(tree, node) do
+    IO.puts("find_erroneous_child")
+    IO.inspect(node)
+    children = Map.get(tree, node).children |> Enum.map(fn(c) -> Map.get(tree, c) end)
+    IO.inspect(children)
+    # Assume that there must be at least three nodes in order to find the
+    # one which is "wrong"
+    # Take the first three, add to a frequency map, and pick the value
+    # greater than two
+    #right
+  end
+
+  def find_balance_weight(filename) do
+    tree = File.stream!(filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(fn(line) -> parse_line(line) end)
+    |> Enum.reduce(%{}, fn(node, acc) -> Map.put(acc, node.name, node) end)
+
+    # TODO optimize: don't read the file twice
+    root = find_root(filename)
+
+    unbalanced_node = find_unbalanced_node(tree, root)
+    find_erroneous_child(tree, unbalanced_node)
   end
 end
--- test/ac/dec07_test.exs
+++ test/ac/dec07_test.exs
@@ -5,21 +5,21 @@ defmodule AC.Dec07Test do
   # problem 1
 
   test "test case 1" do
-    assert AC.Dec07.find_root("/usr/src/myapp/ac/data/dec07_test_input.txt") == ""
+    assert AC.Dec07.find_root("/usr/src/myapp/ac/data/dec07_test_input.txt") == "tknk"
   end
 
   # support
 
   test "it parses a line with children" do
     line = "fwft (72) -> ktlj, cntj, xhth"
-    expected_node = %TreeNode{name: "fwft", weight: 72, children: "ktlj, cntj, xhth"}
+    expected_node = %TreeNode{name: "fwft", weight: 72, children: ["ktlj", "cntj", "xhth"]}
 
     assert AC.Dec07.parse_line(line) == expected_node
   end
 
   test "it parses a line without children" do
     line = "ktlj (57)"
-    expected_node = %TreeNode{name: "ktlj", weight: 57}
+    expected_node = %TreeNode{name: "ktlj", weight: 57, children: []}
 
     assert AC.Dec07.parse_line(line) == expected_node
   end
