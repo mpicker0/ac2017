11925dfb4a5a7b1569a4b81f16efdd66ff4f78fd
Mike Pickering <mpicker0@gmail.com>
2017-12-13 20:37:50 +0000

    Solve Dec 13 problem 1

--- lib/ac/dec13.ex
+++ lib/ac/dec13.ex
@@ -1,7 +1,7 @@
 defmodule AC.Dec13 do
 
   defmodule Layer do
-    defstruct depth: nil, range: nil
+    defstruct depth: nil, range: nil, scanner_location: 0, direction: 1
   end
 
   def parse_line(line) do
@@ -10,12 +10,62 @@ defmodule AC.Dec13 do
   end
 
   # Part 1
-  def find_severity(filename) do
+  def move_scanners(firewall) do
+    Enum.into(firewall, %{}, fn({k,v}) ->
+      {new_direction, new_scanner_location} = cond do
+        v.range == 1 ->
+          {v.direction, v.scanner_location}
+
+        v.scanner_location + v.direction < 0 or
+          v.scanner_location + v.direction == v.range ->
+            {-v.direction, v.scanner_location - v.direction}
+
+        true ->
+          {v.direction, v.scanner_location + v.direction}
+      end
+      {k, %{v | scanner_location: new_scanner_location, direction: new_direction}}
+    end)
+  end
+
+  def find_severity_r(_, step, firewall_length, severity)
+  when step > firewall_length do
+    severity
+  end
+
+  def find_severity_r(firewall, step, firewall_length, severity) do
+    # In the current firewall layer (i.e. step), is the scanner present?
+    # If so, add to the severity
+    new_severity = cond do
+      !Map.has_key?(firewall, step) ->
+        severity
+      Map.get(firewall, step).scanner_location == 0 ->
+        severity + (step * Map.get(firewall, step).range)
+      true ->
+        severity
+    end
+
+    find_severity_r(move_scanners(firewall), step + 1, firewall_length, new_severity)
+  end
+
+  def get_firewall(filename) do
     File.stream!(filename)
     |> Stream.map(&String.trim/1)
     |> Stream.map(fn(line) -> parse_line(line) end)
-    |> Enum.to_list
-    |> IO.inspect
+    |> Enum.reduce(%{}, fn(layer, acc) -> Map.put(acc, layer.depth, layer) end)
+  end
+
+  def find_severity(filename) do
+    firewall = get_firewall(filename)
+    {firewall_length, _} = Enum.max_by(firewall, fn({k,_}) -> k end)
+    find_severity_r(firewall, 0, firewall_length, 0)
+  end
+
+  # Part 2
+
+  def find_shortest_delay(filename) do
+    firewall = get_firewall(filename)
+    IO.puts("TODO")
+    IO.inspect(firewall)
     -1
   end
 
--- test/ac/dec13_test.exs
+++ test/ac/dec13_test.exs
@@ -1,9 +1,46 @@
 defmodule AC.Dec13Test do
   use ExUnit.Case
+  alias AC.Dec13.Layer, as: Layer
 
   # problem 1
   test "example 1" do
     assert AC.Dec13.find_severity("data/dec13_test_input.txt") == 24
+  end
+
+  # support
+  test "move_scanners" do
+    start_state = %{
+      0 => %Layer{depth: 0, range: 3},
+      1 => %Layer{depth: 1, range: 2},
+      2 => %Layer{depth: 2, range: 1}
+    }
+    start_plus_1 = %{
+      0 => %Layer{depth: 0, range: 3, scanner_location: 1},
+      1 => %Layer{depth: 1, range: 2, scanner_location: 1},
+      2 => %Layer{depth: 2, range: 1, scanner_location: 0}
+    }
+    # I'm not happy about having to specify the direction attributes on these,
+    # as I don't care about the internal implementation; maybe there's a way to
+    # ignore this attribute.
+    start_plus_2 = %{
+      0 => %Layer{depth: 0, range: 3, scanner_location: 2},
+      1 => %Layer{depth: 1, range: 2, scanner_location: 0, direction: -1},
+      2 => %Layer{depth: 2, range: 1, scanner_location: 0}
+    }
+    start_plus_3 = %{
+      0 => %Layer{depth: 0, range: 3, scanner_location: 1, direction: -1},
+      1 => %Layer{depth: 1, range: 2, scanner_location: 1},
+      2 => %Layer{depth: 2, range: 1, scanner_location: 0}
+    }
+
+    assert AC.Dec13.move_scanners(start_state) == start_plus_1
+    assert AC.Dec13.move_scanners(start_plus_1) == start_plus_2
+    assert AC.Dec13.move_scanners(start_plus_2) == start_plus_3
+  end
+
+  # problem 2
+  test "example 1 (2)" do
+    #assert AC.Dec13.find_shortest_delay("data/dec13_test_input.txt") == 10
   end
 
 end
