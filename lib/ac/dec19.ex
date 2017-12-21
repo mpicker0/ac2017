bde7be970360d8a3f5d42624b7f0177eaf7831cd
Mike Pickering <mpicker0@gmail.com>
2017-12-21 00:04:47 +0000

    Add parsing to Dec 19 code

--- lib/ac/dec19.ex
+++ lib/ac/dec19.ex
@@ -1,6 +1,24 @@
 defmodule AC.Dec19 do
 
+  defmodule Coord do
+    defstruct x: nil, y: nil
+  end
+
   # Part 1
+  def parse_grid(filename) do
+    File.stream!(filename)
+    |> Stream.map(&String.trim/1)
+    |> Enum.with_index
+    |> Stream.map(fn({s, y}) ->
+         String.codepoints(s)
+         |> Enum.with_index
+         |> Enum.map(fn({b, x}) -> {%Coord{x: x, y: y}, b} end)
+       end)
+    |> Enum.to_list
+    |> List.flatten
+    |> Enum.reduce(%{}, fn({k, v}, acc) -> Map.put(acc, k, v) end)
+  end
+
   def find_path(filename) do
     "ABCDEF" # TODO implement
   end
--- test/ac/dec19_test.exs
+++ test/ac/dec19_test.exs
@@ -1,9 +1,20 @@
 defmodule AC.Dec19Test do
   use ExUnit.Case
+  alias AC.Dec19.Coord, as: Coord
 
   # problem 1
   test "example 1" do
     assert AC.Dec19.find_path("data/dec19_test_input.txt") == "ABCDEF"
+  end
+
+  # support
+  test "grid file parsing" do
+    grid_map = AC.Dec19.parse_grid("data/dec19_test_input_grid.txt")
+
+    assert Map.get(grid_map, %Coord{x: 0, y: 0}) == "a"
+    assert Map.get(grid_map, %Coord{x: 1, y: 0}) == "b"
+    assert Map.get(grid_map, %Coord{x: 0, y: 1}) == "c"
+    assert Map.get(grid_map, %Coord{x: 1, y: 1}) == "d"
   end
 
 end
