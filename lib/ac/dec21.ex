c27122982f3793d061fed3191ac65277c4567273
Mike Pickering <mpicker0@gmail.com>
2017-12-23 00:27:19 +0000

    Finish Dec 21 problem 1

--- lib/ac/dec21.ex
+++ lib/ac/dec21.ex
@@ -2,6 +2,15 @@ defmodule AC.Dec21 do
 
   # Part 1
   @iterations 5
+  @starting_state ".#./..#/###"
+
+  # for debugging
+  def pretty_print(grid) do
+    Enum.map(grid, fn(r) ->
+      IO.puts(Enum.join(r))
+    end)
+    grid
+  end
 
   # input and output are a list of lists
   def rotate_clockwise(grid) do
@@ -12,11 +21,10 @@ defmodule AC.Dec21 do
   end
 
   def flip(grid) do
-    grid
-      |> Enum.map(fn(r) -> Enum.reverse(r) end)
+    grid |> Enum.map(fn(r) -> Enum.reverse(r) end)
   end
 
-  def get_all_rotations(grid, rotations, 0), do: rotations
+  def get_all_rotations(_, rotations, 0), do: rotations
   def get_all_rotations(grid, rotations, remaining) do
     rotated_grid = rotate_clockwise(grid)
     get_all_rotations(rotated_grid, MapSet.put(rotations, rotated_grid), remaining - 1)
@@ -28,19 +36,42 @@ defmodule AC.Dec21 do
     MapSet.union(rotated, flipped_rotated)
   end
 
+  # break up an NxN grid (where N is a multiple of size) into a number of
+  # size x size grids
+  def break_up_grid(in_grid, size) do
+    in_grid
+    |> Enum.map(fn(g)->Enum.chunk_every(g, size) end)
+    |> List.zip
+    |> Enum.map(fn(t) -> Tuple.to_list(t) end)
+    |> Enum.map(fn(g) -> Enum.chunk_every(g, size) end)
+    |> List.zip
+    |> Enum.map(fn(t) -> Tuple.to_list(t) end)
+  end
+
+  def merge_grids(in_grids) do
+    in_grids
+    |> Enum.map(fn(l) ->
+         List.zip(l) |>
+         Enum.map(fn(individual) ->
+           Tuple.to_list(individual)
+           |> Enum.reduce([], fn(list, acc) -> acc ++ list end)
+         end)
+       end)
+    |> Enum.reduce([], fn(list, acc) -> acc ++ list end)
+  end
+
+  # convert a string in the form of ../.# to a list of lists
+  def parse_grid(s) do
+    String.split(s, "/")
+    |> Enum.map(fn(s) -> String.codepoints(s) end)
+  end
+
   # return a keyword list of :in and :out which correspond to the raw input
   # grid and its output.
   def parse_rule(line) do
     %{"in" => in_raw, "out" => out_raw} =
       Regex.named_captures(~r/(?<in>.*) => (?<out>.*)/, line)
-
-    in_rule = in_raw
-      |> String.split("/")
-      |> Enum.map(fn(s) -> String.codepoints(s) end)
-    out_rule = out_raw
-      |> String.split("/")
-      |> Enum.map(fn(s) -> String.codepoints(s) end)
-    [in: in_rule, out: out_rule]
+    [in: parse_grid(in_raw), out: parse_grid(out_raw)]
   end
 
   def rule_map(rules) do
@@ -50,18 +81,33 @@ defmodule AC.Dec21 do
     end)
   end
 
-  def count_on_pixels(filename, iterations \\ @iterations) do
+  def enhance(rules, pixels) when length(pixels) in [2, 3] do
+    Map.get(rules, pixels)
+  end
+  def enhance(rules, pixels) do
+    square_size = if (rem(length(pixels), 2) == 0), do: 2, else: 3
+    pieces = break_up_grid(pixels, square_size)
+    enhanced = Enum.map(pieces, fn(row) ->
+      Enum.map(row, fn(col) -> enhance(rules, col) end)
+    end)
+    merge_grids(enhanced)
+  end
+
+  def iterate_on_pixels(pixels, _, 0), do: pixels
+  def iterate_on_pixels(pixels, rules, iterations) do
+    new_pixels = enhance(rules, pixels)
+    iterate_on_pixels(new_pixels, rules, iterations - 1)
+  end
+
+  def count_on_pixels(filename, iterations \\ @iterations, starting_state \\ @starting_state) do
     all_rules = File.stream!(filename)
     |> Stream.map(&String.trim/1)
     |> Stream.map(fn(line) -> parse_rule(line) end)
     |> rule_map
-    |>IO.inspect
 
-    # TODO
-    # from the starting state, using the rules, continue iterating until
-    # finished with the image
-
-    -1
+    iterate_on_pixels(parse_grid(starting_state), all_rules, iterations)
+    |> List.flatten
+    |> Enum.count(fn(p) -> p == "#" end)
   end
 
 end
--- test/ac/dec21_test.exs
+++ test/ac/dec21_test.exs
@@ -2,9 +2,40 @@ defmodule AC.Dec21Test do
   use ExUnit.Case
 
   # problem 1
-  # test "example 1" do
-  #   assert AC.Dec21.count_on_pixels("data/dec21_test_input.txt", 2) == 12
-  # end
+  test "example 1" do
+    assert AC.Dec21.count_on_pixels("data/dec21_test_input.txt", 2) == 12
+  end
+
+  # integration
+  test "first iteration from example" do
+    start = AC.Dec21.parse_grid(".#./..#/###")
+    rule_1 = AC.Dec21.parse_rule("../.# => ##./#../...")
+    rule_2 = AC.Dec21.parse_rule(".#./..#/### => #..#/..../..../#..#")
+    all_rules = AC.Dec21.rule_map([rule_1, rule_2])
+    expected_out = AC.Dec21.parse_grid("#..#/..../..../#..#")
+
+    assert AC.Dec21.iterate_on_pixels(start, all_rules, 1) == expected_out
+  end
+
+  test "second iteration from example" do
+    start = AC.Dec21.parse_grid("#..#/..../..../#..#")
+    rule_1 = AC.Dec21.parse_rule("../.# => ##./#../...")
+    rule_2 = AC.Dec21.parse_rule(".#./..#/### => #..#/..../..../#..#")
+    all_rules = AC.Dec21.rule_map([rule_1, rule_2])
+    expected_out = AC.Dec21.parse_grid("##.##./#..#../....../##.##./#..#../......")
+
+    assert AC.Dec21.iterate_on_pixels(start, all_rules, 1) == expected_out
+  end
+
+  test "two iterations from example" do
+    start = AC.Dec21.parse_grid(".#./..#/###")
+    rule_1 = AC.Dec21.parse_rule("../.# => ##./#../...")
+    rule_2 = AC.Dec21.parse_rule(".#./..#/### => #..#/..../..../#..#")
+    all_rules = AC.Dec21.rule_map([rule_1, rule_2])
+    expected_out = AC.Dec21.parse_grid("##.##./#..#../....../##.##./#..#../......")
+
+    assert AC.Dec21.iterate_on_pixels(start, all_rules, 2) == expected_out
+  end
 
   # support
   test "rotate clockwise" do
@@ -73,6 +104,13 @@ defmodule AC.Dec21Test do
     assert AC.Dec21.get_all_versions(input) == expected_output
   end
 
+  test "parse grid" do
+    grid_line = "../.#"
+    expected_grid = [[".", "."], [".", "#"]]
+
+    assert AC.Dec21.parse_grid(grid_line) == expected_grid
+  end
+
   test "parse rule" do
     rule_line = "../.# => ##./#../..."
     expected_in = [String.codepoints(".."), String.codepoints(".#")]
@@ -100,6 +138,76 @@ defmodule AC.Dec21Test do
     assert Map.has_key?(all_rules, test2)
     assert Map.has_key?(all_rules, test3)
     assert Map.has_key?(all_rules, test4)
+  end
+
+  test "split a 4x4 grid into four 2x2 grids" do
+    # input looks like: 1 2
+    #                   3 4
+    in_grid = AC.Dec21.parse_grid("0123/4567/89ab/cdef")
+    out_grid_1 = AC.Dec21.parse_grid("01/45")
+    out_grid_2 = AC.Dec21.parse_grid("23/67")
+    out_grid_3 = AC.Dec21.parse_grid("89/cd")
+    out_grid_4 = AC.Dec21.parse_grid("ab/ef")
+    expected_out = [[out_grid_1, out_grid_2], [out_grid_3, out_grid_4]]
+
+    assert AC.Dec21.break_up_grid(in_grid, 2) == expected_out
+  end
+
+  test "split a 6x6 grid into four 3x3 grids" do
+    in_grid = AC.Dec21.parse_grid("0129ab/345cde/678fgh/ijkrst/lmnuvw/opqxyz")
+    out_grid_1 = AC.Dec21.parse_grid("012/345/678")
+    out_grid_2 = AC.Dec21.parse_grid("9ab/cde/fgh")
+    out_grid_3 = AC.Dec21.parse_grid("ijk/lmn/opq")
+    out_grid_4 = AC.Dec21.parse_grid("rst/uvw/xyz")
+    expected_out = [[out_grid_1, out_grid_2], [out_grid_3, out_grid_4]]
+
+    assert AC.Dec21.break_up_grid(in_grid, 3) == expected_out
+  end
+
+  test "split an 8x8 grid into four 4x4 grids" do
+    in_grid = AC.Dec21.parse_grid(
+      "01234567/89abcdef/ghijklmn/opqrstuv/wxyz0987/6543210z/yxwvutsr/qponmlkj")
+    out_grid_1 = AC.Dec21.parse_grid("0123/89ab/ghij/opqr")
+    out_grid_2 = AC.Dec21.parse_grid("4567/cdef/klmn/stuv")
+    out_grid_3 = AC.Dec21.parse_grid("wxyz/6543/yxwv/qpon")
+    out_grid_4 = AC.Dec21.parse_grid("0987/210z/utsr/mlkj")
+    expected_out = [[out_grid_1, out_grid_2], [out_grid_3, out_grid_4]]
+
+    assert AC.Dec21.break_up_grid(in_grid, 4) == expected_out
+  end
+
+  test "merge four 2x2 grids into a 4x4 grid" do
+    in_grid_1 = AC.Dec21.parse_grid("01/45")
+    in_grid_2 = AC.Dec21.parse_grid("23/67")
+    in_grid_3 = AC.Dec21.parse_grid("89/cd")
+    in_grid_4 = AC.Dec21.parse_grid("ab/ef")
+    in_grids = [[in_grid_1, in_grid_2], [in_grid_3, in_grid_4]]
+    expected_out = AC.Dec21.parse_grid("0123/4567/89ab/cdef")
+
+    assert AC.Dec21.merge_grids(in_grids) == expected_out
+  end
+
+  test "merge four 3x3 grids into a 6x6 grid" do
+    in_grid_1 = AC.Dec21.parse_grid("012/345/678")
+    in_grid_2 = AC.Dec21.parse_grid("9ab/cde/fgh")
+    in_grid_3 = AC.Dec21.parse_grid("ijk/lmn/opq")
+    in_grid_4 = AC.Dec21.parse_grid("rst/uvw/xyz")
+    in_grids = [[in_grid_1, in_grid_2], [in_grid_3, in_grid_4]]
+    expected_out = AC.Dec21.parse_grid("0129ab/345cde/678fgh/ijkrst/lmnuvw/opqxyz")
+
+    assert AC.Dec21.merge_grids(in_grids) == expected_out
+  end
+
+  test "merge four 4x4 grids into an 8x8 grid" do
+    in_grid_1 = AC.Dec21.parse_grid("0123/89ab/ghij/opqr")
+    in_grid_2 = AC.Dec21.parse_grid("4567/cdef/klmn/stuv")
+    in_grid_3 = AC.Dec21.parse_grid("wxyz/6543/yxwv/qpon")
+    in_grid_4 = AC.Dec21.parse_grid("0987/210z/utsr/mlkj")
+    in_grids = [[in_grid_1, in_grid_2], [in_grid_3, in_grid_4]]
+    expected_out = AC.Dec21.parse_grid(
+      "01234567/89abcdef/ghijklmn/opqrstuv/wxyz0987/6543210z/yxwvutsr/qponmlkj")
+
+    assert AC.Dec21.merge_grids(in_grids) == expected_out
   end
 
 end
